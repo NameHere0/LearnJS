7be3e9dc11e248be3ece6bae78c975d189cc7f58
NameHere0 <flip.from.2013@gmail.com>
2026-03-24 10:07:56 +0000

    Removed useless test files

--- Test/change.js
+++
@@ -1,7 +0,0 @@
-const btn = document.getElementById("btn");
-const txt = document.getElementById("txt");
-
-btn.addEventListener("click", () => {
-  if (txt.textContent == "hello there") txt.textContent = "HELP ME GODDAMNIT";
-  else txt.textContent = "hello there";
-});
